7b386c0dc953068724ba493424a74fb62c7db595
matheus augusto <math99@outlook.com>
2025-06-16 16:32:40 -0300

    Categoria

---
+++ src/main/java/com/evento/sistemaEventos/entities/Categoria.java
@@ -0,0 +1,33 @@
+package com.evento.sistemaEventos.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
