f1eb70d92cd4366d25f6f3539390477cbd561aa1
matheus augusto <math99@outlook.com>
2025-06-16 16:19:45 -0300

    Projeto criado

---
+++ src/test/java/com/evento/sistemaEventos/SistemaEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evento.sistemaEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
